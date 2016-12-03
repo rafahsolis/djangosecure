4d596ddab6b1671e24af33dcfa95df1fd9a33ebd
Rafael Herrero Solis <rafael@herrerosolis.com>
2016-12-03 14:37:09 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='djangosecure',
+    version='0.0.0',
+    packages=['djangosecure'],
+    url='https://rafael.herrerosolis.com',
+    license='Apache License, Version 2.0',
+    author='Rafael Herrero Solis',
+    author_email='rafael@herrerosolis.com',
+    description='Secure your django site settings'
+)
