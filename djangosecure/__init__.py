82f041ad46b49788dc573f5bf6e0b6f5d7139daa
Rafael Herrero Solis <rafael@herrerosolis.com>
2016-12-03 16:13:56 +0100

    WIP: package structure

---
+++ djangosecure/__init__.py
@@ -0,0 +1,2 @@
+from cryptolib import get_secret_key
+from cryptolib import get_s3_config_new as get_s3_config
--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+import os
 from setuptools import setup
 # from distutils.core import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-secure',
