42e747fd0ce3678d86289edb8043d6525890818d
Rafael Herrero Solis <rafael@herrerosolis.com>
2017-01-22 16:37:58 +0100

    Python 3 compatibility, fix some bugs, unittest

--- setup.py
+++ setup.py
@@ -5,9 +5,27 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+def here(name):
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        name)
+
+
+def read(name, mode='rb', encoding='utf8'):
+    with open(here(name), mode) as fp:
+        return fp.read().decode(encoding)
+
+# Development Status :: 1 - Planning
+# Development Status :: 2 - Pre-Alpha
+# Development Status :: 3 - Alpha
+# Development Status :: 4 - Beta
+# Development Status :: 5 - Production/Stable
+# Development Status :: 6 - Mature
+# Development Status :: 7 - Inactive
+
 setup(
     name='djangosecure',
-    version='v0.0.1',
+    version='v0.0.2',
     packages=['djangosecure'],
     url='https://github.com/rafahsolis/djangosecure',
     download_url='https://github.com/rafahsolis/djangosecure/tarball/v0.0.1',
@@ -16,8 +34,20 @@ setup(
     author_email='rafael@herrerosolis.com',
     keywords=['django', 'secure', 'settings'],
     description='Secure your django sensible settings',
+    long_description=read('README.md'),
+    test_suite='nose.collector',
+    tests_require=['nose', 'six'],
     install_requires=[
         'pycrypto==2.6.1',
         'Django',
-    ]
+        'six==1.10.0',
+        'future==0.16.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
